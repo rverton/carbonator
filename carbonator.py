fb8de37250190ac3248e04a8be0d1f8ea6eae231
Robin Verton <hello@robinverton.de>
2018-07-17 15:10:27 +0200

    Fix issue with newer burp versions (project file); Add export path/format argument; Cleanup code

--- carbonator.py
+++ carbonator.py
@@ -1,123 +1,130 @@
-# Created by Blake Cornell, CTO, Integris Security LLC
-# Integris Security Carbonator - Beta Version - v1.2
-# Released under GPL Version 2 license.
-#
-# See the INSTALL file for installation instructions.
-# 
-# For more information contact us at carbonator at integrissecurity dot com
-# Or visit us at https://www.integrissecurity.com/
+import time
+import os
+
 from burp import IBurpExtender
 from burp import IHttpListener
 from burp import IScannerListener
 from java.net import URL
 from java.io import File
 
-import time
-
 class BurpExtender(IBurpExtender, IHttpListener, IScannerListener):
+
     def registerExtenderCallbacks(self, callbacks):
-	self._callbacks = callbacks
-	self._callbacks.setExtensionName("Carbonator")
-	self._helpers = self._callbacks.getHelpers()
-	self.clivars = None
+        self._callbacks = callbacks
+        self._callbacks.setExtensionName("Carbonator")
+        self._helpers = self._callbacks.getHelpers()
+        self.clivars = None
 
-	self.spider_results=[]
-	self.scanner_results=[]
-	self.packet_timeout=5
+        self.spider_results = []
+        self.scanner_results = []
+        self.packet_timeout = 5
 
-	self.last_packet_seen= int(time.time()) #initialize the start of the spider/scan
+        self.last_packet_seen = int(
+            time.time()
+        )  # initialize the start of the spider/scan
 
-	if not self.processCLI():
-		return None
-	else:
-		self.clivars = True
+        if not self.processCLI():
+            return None
 
-	print "Initiating Carbonator Against: ", str(self.url)
-	#add to scope if not already in there.
-	if self._callbacks.isInScope(self.url) == 0:
-		self._callbacks.includeInScope(self.url)
+        self.clivars = True
 
-	#added to ensure that the root directory is scanned
-	base_request = str.encode(str("GET "+self.path+" HTTP/1.1\nHost: "+self.fqdn+"\n\n"))
-	if(self.scheme == 'HTTPS'):
-		print self._callbacks.doActiveScan(self.fqdn,self.port,1,base_request)
-	else:
-		print self._callbacks.doActiveScan(self.fqdn,self.port,0,base_request)
+        print("Initiating Carbonator against '{}'".format(self.url))
 
-	self._callbacks.sendToSpider(self.url)
-	self._callbacks.registerHttpListener(self)
-	self._callbacks.registerScannerListener(self)
+        # add to scope if not already in there
+        if self._callbacks.isInScope(self.url) == 0:
+            self._callbacks.includeInScope(self.url)
 
-	while int(time.time())-self.last_packet_seen <= self.packet_timeout:
-		time.sleep(1)
-	print "No packets seen in the last", self.packet_timeout, "seconds."
-	print "Removing Listeners"
-	self._callbacks.removeHttpListener(self)
-	self._callbacks.removeScannerListener(self)
-	self._callbacks.excludeFromScope(self.url)
+        # ensure that the root directory is scanned
+        base_request = "GET {} HTTP/1.1\nHost: {}\n\n".format(self.path, self.fqdn)
+        
+        if self.scheme == "HTTPS":
+            print(self._callbacks.doActiveScan(self.fqdn, self.port, 1, base_request))
+        else:
+            print(self._callbacks.doActiveScan(self.fqdn, self.port, 0, base_request))
 
-	print "Generating Report"
-	self.generateReport('HTML')
-	print "Report Generated"
-	print "Closing Burp in", self.packet_timeout, "seconds."
-	time.sleep(self.packet_timeout)
+        self._callbacks.sendToSpider(self.url)
+        self._callbacks.registerHttpListener(self)
+        self._callbacks.registerScannerListener(self)
 
-	if self.clivars:
-		self._callbacks.exitSuite(False)
-		
-	return
+        while int(time.time()) - self.last_packet_seen <= self.packet_timeout:
+            time.sleep(1)
+
+        print("No packets seen in the last {} seconds".format(self.packet_timeout))
+        print("Removing Listeners")
+        self._callbacks.removeHttpListener(self)
+        self._callbacks.removeScannerListener(self)
+        self._callbacks.excludeFromScope(self.url)
+
+        print("Generating Report")
+
+        self.generateReport(self.export)
+
+        print("Report Generated")
+        print("Closing Burp in {} seconds.".format(self.packet_timeout))
+
+        time.sleep(self.packet_timeout)
+
+        if self.clivars:
+            self._callbacks.exitSuite(False)
 
     def processHttpMessage(self, tool_flag, isRequest, current):
-	self.last_packet_seen = int(time.time())
-	if tool_flag == self._callbacks.TOOL_SPIDER and isRequest: #if is a spider request then send to scanner
-		self.spider_results.append(current)
-		print "Sending new URL to Vulnerability Scanner: URL #",len(self.spider_results)
-		if self.scheme == 'https':
-			self._callbacks.doActiveScan(self.fqdn,self.port,1,current.getRequest()) #returns scan queue, push to array
-		else:
-			self._callbacks.doActiveScan(self.fqdn,self.port,0,current.getRequest()) #returns scan queue, push to array
-	return
+        self.last_packet_seen = int(time.time())
+        if (
+            tool_flag == self._callbacks.TOOL_SPIDER and isRequest
+        ):  # if is a spider request then send to scanner
+            self.spider_results.append(current)
+            print("Sending new URL to Vulnerability Scanner: URL #".format(len(self.spider_results)))
+
+            if self.scheme == "https":
+                self._callbacks.doActiveScan(
+                    self.fqdn, self.port, 1, current.getRequest()
+                )  # returns scan queue, push to array
+            else:
+                self._callbacks.doActiveScan(
+                    self.fqdn, self.port, 0, current.getRequest()
+                )  # returns scan queue, push to array
 
     def newScanIssue(self, issue):
-	self.scanner_results.append(issue)
-	print "New issue identified: Issue #",len(self.scanner_results);
-	return
+        self.scanner_results.append(issue)
+        print("New issue identified: Issue #{}".format(len(self.scanner_results)))
 
-    def generateReport(self, format):
-	if format != 'XML':
-		format = 'HTML'	
+    def generateReport(self, filename):
+        _, format = os.path.splitext(filename)
 
-	file_name = 'IntegrisSecurity_Carbonator_'+self.scheme+'_'+self.fqdn+'_'+str(self.port)+'.'+format.lower()
-	self._callbacks.generateScanReport(format,self.scanner_results,File(file_name))
+        if not format in ['xml', 'html']:
+            format = 'xml'
 
-	time.sleep(5)
-	return
+        self._callbacks.generateScanReport(
+            format.upper(), self.scanner_results, File(filename)
+        )
+
+        time.sleep(5)
 
     def processCLI(self):
-	cli = self._callbacks.getCommandLineArguments()
-	if len(cli) < 0:
-		print "Incomplete target information provided."
-		return False
-	elif not cli:
-		print "Integris Security Carbonator is now loaded."
-		print "If Carbonator was loaded through the BApp store then you can run in headless mode simply adding the `-Djava.awt.headless=true` flag from within your shell. Note: If burp doesn't close at the conclusion of a scan then disable Automatic Backup on Exit."
-		print "For questions or feature requests contact us at carbonator at integris security dot com."
-		print "Visit carbonator at https://www.integrissecurity.com/Carbonator"
-		return False
-	elif cli[0] == 'https' or cli[0] == 'http': #cli[0]=scheme,cli[1]=fqdn,cli[2]=port
-		self.scheme = cli[0]
-		self.fqdn = cli[1]
-		self.port = int(cli[2])
-		if len(cli) == 3:
-			self.path = '/'
-		elif len(cli) >= 4:
-			self.path = cli[3]
-		else:
-			print "Unknown number of CLI arguments"
-			return False
-		self.url = URL(self.scheme,self.fqdn,self.port,self.path)
-	else:
-		print "Invalid command line arguments supplied"
-		print cli[0], cli[1], cli[2], cli[3]
-		return False
-	return True
+        cli = self._callbacks.getCommandLineArguments()
+
+        if len(cli) < 0:
+            print("Incomplete target information provided.")
+            return False
+
+        if not cli:
+            print("""Integris Security Carbonator is now loaded.\nIf Carbonator was loaded through the BApp store then you can run in headless mode simply adding the `-Djava.awt.headless=true` flag from within your shell. Note: If burp doesn't close at the conclusion of a scan then disable Automatic Backup on Exit.\nFor questions or feature requests contact us at carbonator at integris security dot com.\nVisit carbonator at https://www.integrissecurity.com/Carbonator""")
+
+            return False
+
+        if cli[0] == "https" or cli[0] == "http":
+            self.scheme = cli[0]
+            self.fqdn = cli[1]
+            self.port = int(cli[2])
+            self.path = cli[3]
+
+            if len(cli) >= 4:
+                self.export = cli[4]
+
+            self.url = URL(self.scheme, self.fqdn, self.port, self.path)
+
+        else:
+            print("Invalid command line arguments supplied")
+            return False
+
+        return True
--- launch_burp.sh
+++ launch_burp.sh
@@ -1,32 +1,26 @@
-# Created by Blake Cornell, CTO, Integris Security LLC
-# Integris Security Carbonator - Beta Version - v1.1
-# Released under GPL Version 2 license.
-# Use at your own risk
+#!/bin/bash
+set -e
 
-if [[ -n $1 && -n $2 && -n $3 ]] #not provide enough parameters to launch carbonator
-then
-	SCHEME=$1
-	FQDN=$2
-	PORT=$3
-	if [[ -n $4 ]]
-	then
-		FOLDER=$4
-	fi
+BURP_PATH=~/tools/BurpSuitePro/burpsuite_pro.jar
+JAVAPATH=/usr/bin/java
 
-	if [[ -n $5 ]]
-	then
-		EMAIL=$5
-		echo Launching Scan against $1://$2:$3$4 EMailing reports to $5
-		java -jar -Xmx1024m ../burp_suite/burpsuite_pro_v1.6.02.jar $SCHEME $FQDN $PORT $FOLDER
-		echo 'Your scan results are attached to this email. Please visit https://www.integrissecurity.com/index.php?resources=Carbonator for more information.' | mutt -s 'Integris Security Carbonator Results' $5 -a IntegrisSecurity_Carbonator_$1_$2_$3.html && rm IntegrisSecurity_Carbonator_$1_$2_$3.html
-	else
-		echo Launching Scan against $1://$2:$3$4
-		java -jar -Xmx1024m ../burp_suite/burpsuite_pro_v1.6.02.jar $SCHEME $FQDN $PORT $FOLDER
-	fi
-else
-	echo Usage: $0 scheme fqdn port path email
-	echo '    'Example: $0 http localhost 80 /folder carbonator@integrissecurity.com
-	echo '    Scan multiple sites: cat scheme_fqdn_port.txt | xargs -L1 '$0
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 scheme fqdn port path output.format"
+	echo " Example: $0 http localhost 80 /path /tmp/result.xml"
 fi
 
-exit
+SCHEME=$1
+FQDN=$2
+PORT=$3
+WPATH=$4
+EXPORT=${5}
+
+tempfile=$(mktemp -t -u carbonator.$FQDN.XXXXXX)
+
+echo "Scanning '$SCHEME://$FQDN:$PORT$WPATH', exporting $EXPORT"
+
+$JAVAPATH -jar -Xmx1024m $BURP_PATH $SCHEME $FQDN $PORT $WPATH $EXPORT --project-file=$tempfile --unpause-spider-and-scanner
+
+# clean up project file
+[ -e $tempfile ] && rm $tempfile
+
